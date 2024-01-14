14127118d63e010f2a5433dc803a5ec0fbfa138b
Mateusz <szachmat2004@gmail.com>
2024-01-14 19:32:54 +0100

    Fixing bug with blue squares for pawns.

--- pawn.cpp
+++ pawn.cpp
@@ -11,36 +11,7 @@ static int previousRow, previousCol;
 
 bool Pawn::move(int fromRow, int fromCol, int toRow, int toCol, int board[][WIDTH], char colors[][WIDTH], char blueRectangles[][8])
 {
-	std::cout << "Blue rectangles: " << static_cast<char>(blueRectangles[toRow][toCol]) << std::endl;
-	if (colors[fromRow][fromCol] == WHITE)
-	{
-		// We cant move backward
-		if (toRow <= fromRow)
-			return false;
-		if (absoluteValue(fromCol, toCol) > 1)
-			return false;
-		if ((fromRow != 1 && absoluteValue(toRow, fromRow) >= 2) || absoluteValue(toRow, fromRow) > 2)
-			return false;
-		if (fromCol - toCol == 1 && board[fromRow + 1][fromCol - 1] == NONE)
-			return false;
-		if (toCol - fromCol == 1 && board[fromRow + 1][fromCol + 1] == NONE)
-			return false;
-	}
-	else if (colors[fromRow][fromCol] == BLACK)
-	{
-		// We cant move backward
-		if (toRow >= fromRow)
-			return false;
-		if (absoluteValue(fromCol, toCol) > 1)
-			return false;
-		if ((fromRow != 6 && absoluteValue(toRow, fromRow) >= 2) || absoluteValue(toRow, fromRow) > 2)
-			return false;
-		if (fromCol - toCol == 1 && board[fromRow - 1][fromCol - 1] == NONE)
-			return false;
-		if (toCol - fromCol == 1 && board[fromRow - 1][fromCol + 1] == NONE)
-			return false;
-	}
-	return true;
+	return blueRectangles[toRow][toCol] == BLUE_RECTANGLE;
 }
 
 void Pawn::displayBlueRectangles(int fromRow, int fromCol, int board[][8], char colors[][8], char blueRectangles[][8])
@@ -60,7 +31,7 @@ void Pawn::displayBlueRectangles(int fromRow, int fromCol, int board[][8], char 
 	{
 		if (board[fromRow - 2][fromCol] == NONE && fromRow == 6)
 			blueRectangles[fromRow - 2][fromCol] = BLUE_RECTANGLE;
-		if (board[fromRow - 1][fromCol] == BLUE_RECTANGLE)
+		if (board[fromRow - 1][fromCol] == NONE)
 			blueRectangles[fromRow - 1][fromCol] = BLUE_RECTANGLE;
 		if (board[fromRow - 1][fromCol + 1] != NONE && colors[fromRow - 1][fromCol + 1] == WHITE)
 			blueRectangles[fromRow - 1][fromCol + 1] = BLUE_RECTANGLE;
