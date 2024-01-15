a401d07ffc95662111ad4c6814e5e8ad7713f635
Mateusz <szachmat2004@gmail.com>
2024-01-15 21:07:44 +0100

    Adding basic move functionality for king

--- king.cpp
+++ king.cpp
@@ -13,10 +13,28 @@ bool King::move(int toRow, int toCol, char blueRectangles[][8])
 
 void King::displayBlueRectangles(int fromRow, int fromCol, int board[][8], char colors[][8], char blueRectangles[][8])
 {
-
+	if (colors[fromRow][fromCol] == WHITE)
+		updateRectangles(BLACK, fromRow, fromCol, board, colors, blueRectangles);
+	else if (colors[fromRow][fromCol] == BLACK)
+		updateRectangles(WHITE, fromRow, fromCol, board, colors, blueRectangles);
 }
 
 void King::updateRectangles(char color, int fromRow, int fromCol, int board[][8], char colors[][8], char blueRectangles[][8])
 {
+	// We start indexing directions from bottom left border
+	int directions[8][2] = { {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1} };
+	for (size_t i = 0; i < 8; i++)
+	{
+		int dirRow = directions[i][0];
+		int dirCol = directions[i][1];
 
+		int toRow = fromRow + dirRow;
+		int toCol = fromCol + dirCol;
+
+		if (toRow <= 7 && toRow >= 0 && toCol <= 7 && toCol >= 0)
+		{
+			if (colors[toRow][toCol] == EMPTY || colors[toRow][toCol] == color)
+				blueRectangles[toRow][toCol] = BLUE_RECTANGLE;
+		}
+	}
 }
