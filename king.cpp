9a0f2d1aa0c3b0af0e6f99ae8c6425df6bfef8ed
Mateusz <szachmat2004@gmail.com>
2024-01-16 19:48:28 +0100

    Refactoring king.cpp file

--- king.cpp
+++ king.cpp
@@ -1,6 +1,9 @@
 #include "game.h"
 #include "king.h"
 
+// We start indexing directions from bottom left border
+int directions[8][2] = { {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1} };
+
 King::King(const char* filename, SDL_Renderer* ren, int bRow, int bCol, char c)
 	: Piece(filename, ren, bRow, bCol, c) 
 {
@@ -29,64 +32,61 @@ bool King::move(int toRow, int toCol, char blueRectangles[][8])
 	return false;
 }
 
-void King::displayBlueRectangles(int fromRow, int fromCol, int board[][8], char colors[][8], 
+void King::displayBlueRectangles(int fromRow, int fromCol, int board[][8], char colors[][8],
 	char blueRectangles[][8], bool forChecks, char whiteColorsSquares[][8], char blackColorsSquares[][8])
 {
-	if (colors[fromRow][fromCol] == WHITE)
-	{
-		updateRectangles(BLACK, fromRow, fromCol, board, colors, blueRectangles, forChecks, blackColorsSquares);
-		int directions[8][2] = { {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1} };
-		for (size_t i = 0; i < 8; i++)
-		{
-			int dirRow = directions[i][0];
-			int dirCol = directions[i][1];
+	char opponentColor = (colors[fromRow][fromCol] == WHITE) ? BLACK : WHITE;
+	updateRectangles(opponentColor, fromRow, fromCol, board, colors, blueRectangles, forChecks,
+		(opponentColor == BLACK) ? blackColorsSquares : whiteColorsSquares);
 
-			int toRow = fromRow + dirRow;
-			int toCol = fromCol + dirCol;
-
-			if (toRow <= 7 && toRow >= 0 && toCol <= 7 && toCol >= 0)
-				whiteColorsSquares[toRow][toCol] = BLUE_RECTANGLE;
-		}
-	}
-	else if (colors[fromRow][fromCol] == BLACK)
-	{
-		updateRectangles(WHITE, fromRow, fromCol, board, colors, blueRectangles, forChecks, whiteColorsSquares);
-		int directions[8][2] = { {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1} };
-		for (size_t i = 0; i < 8; i++)
-		{
-			int dirRow = directions[i][0];
-			int dirCol = directions[i][1];
-
-			int toRow = fromRow + dirRow;
-			int toCol = fromCol + dirCol;
-
-			if (toRow <= 7 && toRow >= 0 && toCol <= 7 && toCol >= 0)
-				blackColorsSquares[toRow][toCol] = BLUE_RECTANGLE;
-		}
-	}
-}
-
-void King::updateRectangles(char color, int fromRow, int fromCol, int board[][8], char colors[][8], 
-	char blueRectangles[][8], bool forChecks, char colorsSquares[][8])
-{
-	// We start indexing directions from bottom left border
-	int directions[8][2] = { {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1} };
 	for (size_t i = 0; i < 8; i++)
 	{
-		int dirRow = directions[i][0];
-		int dirCol = directions[i][1];
+		int toRow, toCol;
+		calculateDestination(fromRow, fromCol, directions[i], toRow, toCol);
 
-		int toRow = fromRow + dirRow;
-		int toCol = fromCol + dirCol;
-
-		if (toRow <= 7 && toRow >= 0 && toCol <= 7 && toCol >= 0)
+		if (isValidSquare(toRow, toCol))
 		{
-			if (colors[toRow][toCol] == EMPTY || colors[toRow][toCol] == color || (forChecks && colors[toRow][toCol] != color))
-				if (checkIfMoveValid(toRow, toCol, colorsSquares))
-					blueRectangles[toRow][toCol] = BLUE_RECTANGLE;
+			char(*targetColorsSquares)[8] = (colors[fromRow][fromCol] == WHITE) ? whiteColorsSquares : blackColorsSquares;
+			targetColorsSquares[toRow][toCol] = BLUE_RECTANGLE;
 		}
 	}
 }
+
+void King::updateRectangles(char color, int fromRow, int fromCol, int board[][8], char colors[][8],
+	char blueRectangles[][8], bool forChecks, char colorsSquares[][8])
+{
+	for (size_t i = 0; i < 8; i++)
+	{
+		int toRow, toCol;
+		calculateDestination(fromRow, fromCol, directions[i], toRow, toCol);
+
+		if (isValidSquare(toRow, toCol))
+		{
+			if (isValidMove(color, toRow, toCol, colors, forChecks, colorsSquares))
+				blueRectangles[toRow][toCol] = BLUE_RECTANGLE;
+		}
+	}
+}
+
+void King::calculateDestination(int fromRow, int fromCol, int direction[], int& toRow, int& toCol)
+{
+	int dirRow = direction[0];
+	int dirCol = direction[1];
+	toRow = fromRow + dirRow;
+	toCol = fromCol + dirCol;
+}
+
+bool King::isValidSquare(int row, int col)
+{
+	return (row >= 0 && row <= 7 && col >= 0 && col <= 7);
+}
+
+bool King::isValidMove(char color, int toRow, int toCol, char colors[][8], bool forChecks, char colorsSquares[][8])
+{
+	return (colors[toRow][toCol] == EMPTY || colors[toRow][toCol] == color || (forChecks && colors[toRow][toCol] != color))
+		&& checkIfMoveValid(toRow, toCol, colorsSquares);
+}
+
 
 bool King::checkIfMoveValid(int toRow, int toColumn, char colorsSquares[][8])
 {
--- king.h
+++ king.h
@@ -21,4 +21,7 @@ public:
 	void updateRectangles(char color, int fromRow, int fromCol, int board[][8], char colors[][8], 
 		char blueRectangles[][8], bool forChecks, char colorsSquares[][8]);
 	bool checkIfMoveValid(int toRow, int toColumn, char colorsSquares[][8]);
+	void calculateDestination(int fromRow, int fromCol, int direction[], int& toRow, int& toCol);
+	bool isValidSquare(int row, int col);
+	bool isValidMove(char color, int toRow, int toCol, char colors[][8], bool forChecks, char colorsSquares[][8]);
 };
